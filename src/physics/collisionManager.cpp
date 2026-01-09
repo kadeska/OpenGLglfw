3eb6d0ab5bf25c8ec7b17a3aa70187510b66fa80
Kadeska <kadeska23@gmail.com>
2026-01-08 22:54:04 -0700

    uncommented sphere to sphere check

--- src/physics/collisionManager.cpp
+++ src/physics/collisionManager.cpp
@@ -10,10 +10,10 @@ CollisionManager::CollisionManager()
 bool CollisionManager::checkForCollisions(GameObject* _gameObject1, GameObject* _gameObject2)
 {
 	success = CollisionTests::sphereToPlane(_gameObject1->getCollisionSphere(), glm::vec3(0,0,0), glm::vec3(0, 1, 0));
-	/*if (_gameObject2 != nullptr) 
+	if (_gameObject2 != nullptr) 
 	{
 		success = CollisionTests::sphereToSphere(_gameObject1->getCollisionSphere(), _gameObject2->getCollisionSphere());
-	}*/
+	}
 	
 
 
